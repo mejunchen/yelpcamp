a3e368277994e51d93a341aedaf0ac09cc2c9295
opennetnz <jun.opennet@gmail.com>
2018-01-15 08:27:17 +0000

    Add time since created with Moment JS

--- models/comment.js
+++ models/comment.js
@@ -3,6 +3,7 @@ mongoose.Promise = global.Promise;
 
 var commentSchema = mongoose.Schema({
     text: String,
+    createdAt: {type: Date, default: Date.now },
     author: {
         id: {
             type: mongoose.Schema.Types.ObjectId,
